c853a24331b6e12fc42ccff06ad4c847e941502f
Grosu Antonio <codevs@192-168-0-130.rdsnet.ro>
2024-04-23 01:45:43 +0300

    edit mobile message

--- app/layout.js
+++ app/layout.js
@@ -6,9 +6,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-gray-200">
-        <div className="md:hidden w-full h-screnn flex items-center justify-center">
+        <div className="md:hidden w-full h-screen flex items-center justify-center">
           <h1 className="text-4xl">
-            Unfortunately,the Teacher Panel cannot pe accessed from mobile
+            Unfortunately,the Teacher Panel cannot be accessed from mobile
             devices.
           </h1>
         </div>
